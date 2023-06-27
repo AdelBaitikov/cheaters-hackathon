b98776ba5f5219e9e8f7a614228522728a034361
Baitikov Adel <adelbajtikob259@gmail.com>
2023-06-27 19:42:51 +0600

    Создал новый класс TaskModel

---
+++ TaskManager/TaskManager/Enums/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Enums
+{
+    public enum Status
+    {
+        Planning = 1,
+        Complete = 2,
+        Delayed = 3,
+    }
+}
