42ec0dbf4999c30dad908b8ffc6a5d5f1fc543ce
Baitikov Adel <adelbajtikob259@gmail.com>
2023-06-27 19:56:36 +0600

    Поменял класс

--- TaskManager/TaskManager/Models/TaskModel.cs
+++ TaskManager/TaskManager/Models/TaskModel.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TaskManager.Enums;
 
-namespace TaskManager.Models
-{
+namespace TaskManager.Models;
+
     public class TaskModel
     {
-
-    }
+        public long Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public DateTime DeadLine { get; set; }
+        public Priority Priority { get; set; }
+        public Status Status { get; set; }
 }
