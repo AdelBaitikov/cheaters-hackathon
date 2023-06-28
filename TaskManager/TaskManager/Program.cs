9cbd34c7b7da3f0da5cd6e107f5e5bf93c9640b1
Baitikov Adel <adelbajtikob259@gmail.com>
2023-06-28 19:07:46 +0600

    соединил с sql сервером

--- TaskManager/TaskManager/Program.cs
+++ TaskManager/TaskManager/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+string connectionString = "Data Source=.;Initial Catalog=TaskModels;Trusted_Connection=true; encrypt option";
+
+using (SqlConnection connection = new SqlConnection(connectionString)) ;
+
+    
