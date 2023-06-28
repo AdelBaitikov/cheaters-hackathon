07ca581fc298384317a4c6cdb1fdeca4e7cdf049
Baitikov Adel <adelbajtikob259@gmail.com>
2023-06-28 18:56:45 +0600

    Сделал норм БазуДанных

--- Database.sql
+++ Database.sql
@@ -1,1 +1,15 @@
-SELECT*FROM [DataBase]
+CREATE DATABASE TaskManagerDb 
+
+GO
+
+USE TaskManagerDb
+
+CREATE TABLE TaskModels
+(
+	Id INTEGER,
+	Title NVARCHAR(150),
+	[Description] NVARCHAR(500),
+	[DateTime] DateTime,
+	[Priority] TINYINT,
+	[Status] TINYINT,
+)
